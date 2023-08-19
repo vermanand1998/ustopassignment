0e1be7bed608311b5a1c7c2419a92fdc6571cfa2
vermanand1998 <vermanand1998@gmail.com>
2023-08-20 04:50:35 +0530

    first-change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, Renderer2 } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,23 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   sidebarOpen = false;
+  selectedValue="";
+  isMobile = false;
+  private mediaSub: Subscription;
   activeStates: boolean[] = [false, false, false, false];
-  constructor(private renderer: Renderer2,private router: Router) { }
+  constructor(private renderer: Renderer2,private router: Router,private mediaObserver: MediaObserver) {
+    this.mediaSub = this.mediaObserver.asObservable().subscribe((changes) => {
+      this.isMobile = changes.some((change) => change.mqAlias === 'xs');
+    });
+   }
 
   ngOnInit(): void {
+    this.selectedValue="Assessment";
     this.navigateToOtherComponent('assessment',2);
+  }
+
+  ngOnDestroy() {
+    this.mediaSub.unsubscribe();
   }
   openSidebar() {
     if (!this.sidebarOpen) {
@@ -31,17 +45,22 @@ export class AppComponent {
   }
 
   navigateToOtherComponent(key: string,menuItem: number) {
+    this.closeSidebar();
     // Reset active state for all menu items
     this.activeStates = this.activeStates.map(_ => false);
     // Set active state for the clicked menu item
     this.activeStates[menuItem - 1] = true;
     if(key=="Dashboard"){
+      this.selectedValue="Dashboard"
       this.router.navigate(['/dashboard']); // Replace 'target-route' with your actual route path
     }else if(key=="libr"){
+      this.selectedValue="My Library"
       this.router.navigate(['/my-library']); // Replace 'target-route' with your actual route path
     }else if(key=="status"){
+      this.selectedValue="Round Status"
       this.router.navigate(['/round-status']); // Replace 'target-route' with your actual route path
     }else if(key=="assessment"){
+      this.selectedValue="Assessment"
       this.router.navigate(['/assessment']); // Replace 'target-route' with your actual route path
     }
   }
